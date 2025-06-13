0b1cda559c04701555a5ad7b1ac05ab477284d2e
pv <serjess@proton.me>
2025-06-14 01:30:34 +0300

    exp

--- src/components/SingleComponents/Header/UserStats.js
+++ src/components/SingleComponents/Header/UserStats.js
@@ -43,9 +43,14 @@ const UserStats = () => {
 	useEffect(() => {
 		const discriminant = 355216 + 16 * experience;
 		const currentLvl = Math.max(1, Math.floor((Math.sqrt(discriminant) - 588) / 8));
-
 		setLevel(currentLvl);
-		setToNextLevel(8 * currentLvl + 592);
+		const xpAtLevelStart =  4 * level * level + 588 * level - 592;
+		const xpInLevel = experience - xpAtLevelStart;
+
+
+		const xpNeededTotal = 8 * level + 592;
+		const xpToNextLevel = xpNeededTotal - xpInLevel;
+		setToNextLevel(xpToNextLevel);
 	}, [experience]);
 
 	const userLvlStats = [
